5a69a4b972daca13be09e2f2e4314101587e83e1
sajad <sajadsharghi584@gmail.com>
2024-06-15 03:33:03 -0700

    EFT

--- Homework2.php
+++ Homework2.php
@@ -6,6 +6,23 @@
     <title>Homework 2</title>
 </head>
 <body>
-    
+    <h1>
+    <?php echo "do you read song of ice and fire ? "?>
+    </h1>
+    <?php
+    session_start();
+    if(isset($_POST["True1"]))
+    {
+     echo $_SESSION["CNT"] += 0;
+    }
+    else
+    {
+     echo $_SESSION["CNTF"] += 0;
+    }
+    ?>
+    <form method="post" id="Q&A" action="<?php  echo $_SERVER['PHP_SELF'] ?>">
+<input type="submit" value="yes" name="True1">
+<input type="submit" value="No" name="False1">
+    </form>
 </body>
 </html>
