26bf44554b5486ff7f741210fce63953b1512e2a
sajad <sajadsharghi584@gmail.com>
2024-06-15 03:48:01 -0700

    FFF

--- Homework2.php
+++ Homework2.php
@@ -9,20 +9,23 @@
     <h1>
     <?php echo "do you read song of ice and fire ? "?>
     </h1>
-    <?php
-    session_start();
-    if(isset($_POST["True1"]))
-    {
-     echo $_SESSION["CNT"] += 0;
-    }
-    else
-    {
-     echo $_SESSION["CNTF"] += 0;
-    }
-    ?>
     <form method="post" id="Q&A" action="<?php  echo $_SERVER['PHP_SELF'] ?>">
 <input type="submit" value="yes" name="True1">
 <input type="submit" value="No" name="False1">
     </form>
+    <?php
+    session_start();
+    if(isset($_POST["True1"]))
+    {
+     $_SESSION["CNT"] += 1;
+    }
+    else if(isset($_POST["False1"]))
+    {
+      $_SESSION["CNTF"] += 1;
+    }
+    echo "The number of user who read the Song of the ice and fire " .' '. $_SESSION["CNT"];
+    echo"</br>";
+    echo "The number of user who did not read the Song of the ice and fire " .' '. $_SESSION["CNTF"];
+    ?>
 </body>
 </html>
